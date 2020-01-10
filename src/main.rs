ba55943cb30a77e1bd93c349fcf733ba4310b1bd
Justin Fargnoli <justinfargnoli@gmail.com>
2020-01-10 16:39:23 -0500

    Adding the sqlparser example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let dialect = GenericDialect {}; // or AnsiDialect
+
+    let sql = "SELECT a, b, 123, myfunc(b) \
+               FROM table_1 \
+               WHERE a > b AND b < 100 \
+               ORDER BY a DESC, b";
+
+    let ast = Parser::parse_sql(&dialect, sql.to_string()).unwrap();
+
+    println!("AST: {:?}", ast);
 }
